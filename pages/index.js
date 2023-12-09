c1fee3b7d9a2159382cea82e341926344be8c7ab
MinHui02 <153159901+MinHui02@users.noreply.github.com>
2023-12-09 21:03:22 +0800

    github

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import React from "react";
+import HeroSection from "@/pages/component/HeroSection";
+import Project from "./component/Project";
 
 export default function Home(){
   return(
-    <div>h1</div>
+    <div className="flex min-h-screen flex-col">
+      <HeroSection />
+    </div>
   );
 }
