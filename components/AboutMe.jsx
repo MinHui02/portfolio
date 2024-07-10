8bd6dfb17ce41ee1dffdcca508532bf31632c92a
MinHui02 <153159901+MinHui02@users.noreply.github.com>
2024-07-10 22:02:47 +0800

    Update AboutMe.jsx

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -10,14 +10,13 @@ const TAB_DATA = [
         id: "skills",
         content: (
           <ul className="list-disc pl-2">
-                <li>React</li>
-                <li>Next.js</li>
-                <li>JavaScript</li>
-                <li>Solidity</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Tailwind CSS</li>
                 <li>Python</li>
+                <li>C / C++</li>
+                <li>Java(OOP/Concurrent Programming)</li>
+                <li>SAS</li>
+                <li>R Programming</li>
+                <li>SAS Enterprise Miner</li>
+                <li>Microsaft Power BI</li>
           </ul>
         ),
     },
@@ -71,7 +70,7 @@ const AboutMe = () => {
                 </div>
                 <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                     <h1 className="text-4xl font-bold text-white mb-4">About Me</h1>
-                    <p className="text-base text-justify lg:text-lg">I am currently pursuing a degree in Cyber Security at APU, where I have been honing my skills in various technologies. I am familiar in frontend development, with expertise in React, Next.js, HTML, CSS, and I am skilled in creating visually appealing and user-friendly interfaces using Tailwind CSS.
+                    <p className="text-base text-justify lg:text-lg">I am currently pursuing a degree in Computer Science with a specialism in Data Analytics at APU.
                         Additionally, I have ventured into the world of blockchain development, particularly with Solidity, allowing me to build smart contracts for decentralized applications.
                         My passion for technology and problem-solving has led me to actively participate in 11 hackathons this year, where I primarily serve as a Front End Developer. However, I am not limited to frontend tasks and often take on backend responsibilities, demonstrating my versatility and commitment to delivering complete solutions. </p>
                     <div className="flex flex-row mt-8">
