8f7371c05889d380caefc7172bb048725ec094c9
MinHui02 <153159901+MinHui02@users.noreply.github.com>
2025-10-24 17:13:00 +0800

    Update AchievementsSection.jsx

--- components/AchievementsSection.jsx
+++ components/AchievementsSection.jsx
@@ -3,15 +3,11 @@ import React from "react";
 const achievementsList = [
     {
         metric: "Projects",
-        value: "10+",
-    },
-    {
-        metric: "Awards",
-        value: "3+",
+        value: "1",
     },
     {
         metric: "Years",
-        value: "1",
+        value: "3",
     }
 ];
 
